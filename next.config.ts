251e2015d84433094402a0e2a44f4ed9cf9458f6
Sneha Varsha <nuthalapatisnehavarsha11@gmail.com>
2025-09-23 23:46:42 -0500

    Portfolio for Sneha Varsha Nuthalapati

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@
 const nextConfig = {
     /* config options here */
     reactStrictMode: true,
+    output: 'export',
+    trailingSlash: true,
     images: {
+        unoptimized: true,
         domains: ["example.com", "upload.wikimedia.org", "logo.clearbit.com"],
     },
     eslint: {
