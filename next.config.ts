1b7579bb70a9c56c50e95c15d1bad63e7a62c653
Sneha Varsha <nuthalapatisnehavarsha11@gmail.com>
2025-09-26 23:38:45 -0500

    Fix GitHub Pages configuration

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,6 @@ const nextConfig = {
     reactStrictMode: true,
     output: 'export',
     trailingSlash: true,
-    basePath: '/sneha-portfolio',
-    assetPrefix: '/sneha-portfolio/',
     images: {
         unoptimized: true,
         domains: ["example.com", "upload.wikimedia.org", "logo.clearbit.com"],
